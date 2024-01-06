e5f4de20844659fde5c0f2ef84a907dde9ec41b6
Çağrı <52715393+cagribolat@users.noreply.github.com>
2024-01-06 15:18:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
         body {
-            text-align: center;
+            text-align: top;
         }
 
         h1 {
